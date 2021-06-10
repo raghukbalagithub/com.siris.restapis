340ec6ac0080a8ec045b8ec8a350408cec013c04
raghukbalagithub <raghukbala@gmail.com>
2021-06-11 01:35:16 +0530

    class name change - refactoring

--- src/test/java/com/siris/restapis/resttemplatetests/Site1Tests.java
+++ src/test/java/com/siris/restapis/resttemplatetests/Site1Tests.java
@@ -1,23 +1,18 @@
 package com.siris.restapis.resttemplatetests;
 
-import com.siris.restapis.resttemplate.GetRestTemplate;
+import com.siris.restapis.resttemplate.RestTemplateExecutor;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class Site1Tests {
 
@@ -30,19 +25,18 @@ public class Site1Tests {
     private RestTemplate restTemplate;
     private String token;
 
-    @BeforeClass
-    public void beforeClass() {
-        LOG.info("GETTING THE REST TEMPLATE OBJECT");
-        this.restTemplate = new GetRestTemplate().getRestTemplate();
+    @BeforeTest
+    public void beforeTest() {
+        LOG.info("IN BEFORE TEST");
     }
 
-    @AfterClass
-    public void afterClass() {
-
-    }
-
-    @BeforeMethod
+    @BeforeClass(alwaysRun = true)
     public void fetchToken() {
+        LOG.info("IN BEFORE CLASS");
+        LOG.info("GETTING THE REST TEMPLATE OBJECT");
+        this.restTemplate = new RestTemplateExecutor().getRestTemplate();
+
+        LOG.info("GETTING THE AUTHENTICATION TOKEN");
         String uri = TARGET_URL.concat(AUTH_ENDPOINT);
 
         String data = "{\"username\": \"admin\", \"password\": \"password123\"}";
@@ -57,25 +51,64 @@ public class Site1Tests {
         this.token = curateTokenResponseAndGiveToken(responseEntity.getBody());
     }
 
-    @Test
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() {
+        LOG.info("IN BEFORE METHOD");
+    }
+
+    @Test(alwaysRun = true)
     public void postMethod() {
         LOG.info("TESTING POST METHOD");
-        LOG.info("Token: {}",this.token);
     }
 
     @Test
-    public void getMethod() {
+    public void getAllBookingIDs() {
+        LOG.info("TESTING GET METHOD");
 
+        String uri = TARGET_URL.concat("/booking");
+
+        ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(uri, String.class);
+
+        LOG.info("TOKEN RESPONSE BODY: {}", responseEntity.getBody());
+
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(responseEntity.getBody());
+            JSONArray jsonArray = (JSONArray)obj;
+            for(Object obj1 : jsonArray) {
+                LOG.info(obj1.toString());
+                JSONObject obj2 = (JSONObject)obj1;
+                String id = obj2.get("bookingid").toString();
+                LOG.info("Details for bookingid: {}", id);
+                getBookingIDDetails(id);
+            }
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    @Test
+    @Test(dependsOnMethods = "postMethod")
     public void putMethod() {
-
+        LOG.info("TESTING THE PUT METHOD");
     }
 
     @Test
     public void deleteMethod() {
+        LOG.info("TESTING THE DELETE METHOD");
+    }
 
+    private void getBookingIDDetails(String id) {
+        String uri = TARGET_URL.concat("/booking/{bookingid}");
+        //uri = uri.concat("/").concat(bookingid);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        String bookingid = id;
+        ResponseEntity<String> responseEntity = this.restTemplate.exchange(uri, HttpMethod.GET, entity, String.class, bookingid);
+        LOG.info("TOKEN RESPONSE BODY: {}", responseEntity.getBody());
     }
 
     private String curateTokenResponseAndGiveToken(String tokenResponseEntity) {
@@ -91,5 +124,10 @@ public class Site1Tests {
         }
 
         return fetchedToken;
+    }
+
+    @AfterClass()
+    public void afterClass() {
+        LOG.info("IN AFTER CLASS");
     }
 }
